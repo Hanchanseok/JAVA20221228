3c7a85f7d3697be990a3cf065940346a04d49617
Hanchanseok <hcs2804@naver.com>
2022-12-30 18:42:28 +0900

    02

--- src/Ch02/C03변수자료형.java
+++ src/Ch02/C03변수자료형.java
@@ -6,11 +6,11 @@ public class C03변수자료형 {
 		//leftValue=RightValue - 대입연산자(=)를 기준으로 왼쪽은 lv(공간) 오른쪽은 rv(값)
 		//lv(공간)=rv(값)
 		
-		int num1;
-		num1=10;
+		int num1;	//4byte정수 공간 생성+num1 이름 부여 (변수 선언 or 정의)
+		num1=10;	// 10값을 상수Pool에 저장 하고 num1공간에 복사(대입)
 		int num2=4;
 		int num3=num1+num2;
-		System.out.println(num3); 
+		System.out.println(num3);
 
 	}
 
