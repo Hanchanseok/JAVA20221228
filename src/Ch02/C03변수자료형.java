a6f0a9fcdccb2c7796d976b487a5fe9096ac2914
Hanchanseok <hcs2804@naver.com>
2022-12-30 18:47:32 +0900

    02

--- src/Ch02/C03변수자료형.java
+++ src/Ch02/C03변수자료형.java
@@ -7,10 +7,10 @@ public class C03변수자료형 {
 		//lv(공간)=rv(값)
 		
 		int num1;	//4byte정수 공간 생성+num1 이름 부여 (변수 선언 or 정의)
-		num1=10;	// 10값을 상수Pool에 저장 하고 num1공간에 복사(대입)
-		int num2=4;
-		int num3=num1+num2;
-		System.out.println(num3);
+		num1=10;	// 10값을 상수Pool에 저장(int) 하고 num1공간에 복사(대입)
+		int num2=4; // 4값을 상수Pool에 저장(int) 하고 새로 만들어지는 4byte 정수공간 num2에 초기화
+		int num3=num1+num2;  // num1의 값 + num2의 값 을 4byte정수공간 num3에 초기화
+		System.out.println(num3);   // num3안의 값을 print
 
 	}
 
