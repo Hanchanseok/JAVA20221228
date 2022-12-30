4a79ae05a052b3ef21548b09e6fc6758499ac7e4
Hanchanseok <hcs2804@naver.com>
2022-12-30 18:33:53 +0900

    02

--- src/Ch02/C01진수.java
+++ src/Ch02/C01진수.java
@@ -18,6 +18,18 @@ public class C01진수 {
 		//%d	:10진정수 서식문자
 		//%o	:8진수 서식문자
 		//%x	:16진수 서식문자
+		System.out.printf("10진수 : %d\n", 0b1111);	//2진수(0b : 2진수를 의미하는 접두사)
+		System.out.printf("10진수 : %d\n", 173);		//10진수
+		System.out.printf("10진수 : %d\n", 0255);	//8진수 (0 : 8진수를 의미하는 접두사)
+		System.out.printf("10진수 : %d\n", 0xAD);	//16진수(0x:16진수를 의미하는 접두사)
+		
+		System.out.printf("8진수 : %o\n", 173);		//10진수
+		System.out.printf("8진수 : %o\n", 0255);		//8진수 (0 : 8진수를 의미하는 접두사)
+		System.out.printf("8진수 : %o\n", 0xAD);		//16진수(0x:16진수를 의미하는 접두사)
+		
+		System.out.printf("16진수 : %x\n", 173);		//10진수
+		System.out.printf("16진수 : %x\n", 0255);	//8진수 (0 : 8진수를 의미하는 접두사)
+		System.out.printf("16진수 : %x\n", 0xAD);	//16진수(0x:16진수를 의미하는 접두사)
 		
 
 	}
