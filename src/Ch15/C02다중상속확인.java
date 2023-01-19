ee4135ae13e6b92e26fd74e74004093f5e710bf1
Hanchanseok <hcs2804@naver.com>
2023-01-19 18:44:05 +0900

    15

---
+++ src/Ch15/C02다중상속확인.java
@@ -0,0 +1,28 @@
+package Ch15;
+
+//class A{}
+//class B{}
+//class C{}
+
+//class A{}
+//class B{}
+//class C extends A,B{}	//다중상속 x
+
+//interface A{}
+//interface B{}
+//class C implements A,B{}  //다중상속 o
+
+interface A{}
+interface B{}
+class C{}
+class D extends C implements A,B{}	 //extends가 implements보다 먼저 와야한다.
+
+
+public class C02다중상속확인 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
