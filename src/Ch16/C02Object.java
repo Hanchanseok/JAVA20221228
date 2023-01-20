4b7fa712a98a1fddf030213396b51d004fa05f9c
Hanchanseok <hcs2804@naver.com>
2023-01-20 17:51:52 +0900

    16

--- src/Ch16/C02Object.java
+++ src/Ch16/C02Object.java
@@ -1,13 +1,23 @@
 package Ch16;
 
-class Simple
+class C02Simple
 {
 	int x;
 	int y;
-	Simple(int x, int y) {
+	C02Simple(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
+	@Override
+	public boolean equals(Object obj)  //object obj = ob2; //Upcasting
+	{
+		if(obj instanceof C02Simple) {
+			C02Simple down = (C02Simple)obj;
+			return this.x==down.x && this.y==down.y;
+		}
+		return false;
+	}
+	
 }
 
 class B{}
@@ -15,9 +25,9 @@ class C{}
 public class C02Object {
 
 	public static void main(String[] args) {
-		Simple ob1 = new Simple(10, 20);
-		Simple ob2 = new Simple(30, 40);
-		Simple ob3 = new Simple(10, 20);
+		C02Simple ob1 = new C02Simple(10, 20);
+		C02Simple ob2 = new C02Simple(30, 40);
+		C02Simple ob3 = new C02Simple(10, 20);
 		System.out.println(ob1.equals(ob2));
 		System.out.println(ob1.equals(ob3));
 		System.out.println(ob2.equals(ob3));
